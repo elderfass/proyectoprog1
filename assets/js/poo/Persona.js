b70937f9b3158278f12673a9409e71059112ccf6
elderfass <120078283+elderfass@users.noreply.github.com>
2023-11-24 04:27:58 -0500

    vercion 0.1

--- assets/js/poo/Persona.js
+++ assets/js/poo/Persona.js
@@ -1,81 +1,98 @@
 // modelo
- class Persona {
-   
-    static conteo=0;
-    personas =[];
-    constructor(id='sin id',nombre='sin nombre',email='sin email',clave='sin clave',rol='sin rol') {
+class Persona {
+    constructor(id, nombre, email, clave, rol) {
         this.id = id;
-        this.nombre =nombre;
+        this.nombre = nombre;
         this.email = email;
         this.clave = clave;
         this.rol = rol;
-        Persona.conteo++;
-    }
-   
-
-    editar(id,nombre,email,clave,rol) {
-       this.id = id;
-       this.nombre = nombre;
-       this.email = email;
-       this.clave = clave;
-       this.rol = rol;
-       
-    }
-    guardar(persona){
-        this.personas.push({persona});
-    }
-  
-    llenarObjeto() {       
-            document.getElementById("tableD").innerHTML +=
-            "<tr>"+
-                "<td>" +this.id+ "</td>"+
-                "<td>"+this.nombre+"</td>"+
-                "<td>"+this.email+"</td>"+
-                "<td>"+this.clave+"</td>"+
-                "<td>"+this.rol+"</td>"+
-                "<td><button onclick='Editar();' class='btn btn-primary mx-2'> editar </button>"+
-                "<button onclick='Eliminar();' class='btn btn-danger'> Eliminar </button></td>"+
-            "</tr>";
     }
 
-    // actualizarDatos(array){
-    //     for (let index = 0; index <= array.length; index++) {
-    //         if (array[index].id==document.getElementById('cc').value) {
-    //             document.getElementById("tableD").innerHTML+= 
-    //             "<tr>"+
-    //             "<td>" +this.id+ "</td>"+
-    //             "<td>"+this.nombre+"</td>"+
-    //             "<td>"+this.email+"</td>"+
-    //             "<td>"+this.clave+"</td>"+
-    //             "<td>"+this.rol+"</td>"+
-    //             "<td><button onclick='Editar();' class='btn btn-primary mx-2'> editar </button>"+
-    //             "<button onclick='Eliminar();' class='btn btn-danger'> Eliminar </button></td>"+
-    //             "</tr>";
-    //         }
-            
-    //     }
-            
-            
-    //     }
-    }
-
+    llenarTabla(array, table, index) {
+        var newRow = table.insertRow(index);
+        for (var j = 0; j < 5; j++) {
+            var cell = newRow.insertCell(j);
+            switch (j) {
+                case 0:
+                    cell.innerText = array[index].id;
+                    break;
+                case 1:
+                    cell.innerText = array[index].nombre;
+                    break;
+                case 2:
+                    cell.innerText = array[index].email;
+                    break;
+                case 3:
+                    cell.innerText = array[index].clave;
+                    break;
+                case 4:
+                    cell.innerText = array[index].rol;
+                    break;
+            }
+        }
     
+        var editarBtn = document.createElement("button");
+        editarBtn.innerText = "Editar";
+        editarBtn.classList.add("btn", "btn-primary", "mx-2");
+        editarBtn.addEventListener('click', (function (i) {
+            return function () {
+                editar(array[i], array, i);
+            };
+        })(index));
+    
+        var eliminarBtn = document.createElement("button");
+        eliminarBtn.innerText = "Eliminar";
+        eliminarBtn.classList.add("btn", "btn-danger");
+        eliminarBtn.addEventListener('click', (function (i) {
+            return function () {
+                eliminar(array[i], array, i);
+            };
+        })(index));
+        
+        var cellOpciones = newRow.insertCell(5);
+        cellOpciones.appendChild(editarBtn);
+        cellOpciones.appendChild(eliminarBtn);
+    }
 
+    editarPersona(personad, array, table) {
+        for (let index = 0; index < array.length; index++) {
+            if (personad.email === array[index].email ){
+                if (personad.id === array[index].id && personad.nombre === array[index].nombre && personad.clave === array[index].clave && personad.rol === array[index].rol) {
+                    alert("No se puede actualizar");
+                    return;
+                }else{
+                    array[index] = personad;
+                    table.deleteRow(index);
+                    this.llenarTabla(array, table, index);
+                    alert("Edición realizada con éxito");
+                    return;
+                }
+            }
+        }
+        return true;
+    }
+    
+    eliminarPersona(personad, array, table) {
+        for (let index = 0; index < array.length; index++) {
+            if (personad.email === array[index].email) {
+                array.splice(index, 1);
+                table.deleteRow(index);
+                contador--; // Actualizar el contador
+                alert("Eliminación realizada con éxito");
+                return;
+            }
+        }
+    }
 
-
-
-
-
-
-        // contador++;
-        // var index = contador;
-
-
-    //         "<button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#noc' onclick='actualizarPersona(" +
-    //         index +
-    //         ")'>Actualizar</button>" +
-    //         "</td><td>" +
-    //         "<button type='button' class='btn btn-danger' onclick='eliminarPersona(" +
-    //         index +
-    //         ")'>Eliminar</button> " +
-    //         "</td></tr>";
+    buscar(array, index, rows) {
+        for (var i = 0; i < rows.length; i++) {
+            var cells = rows[i].getElementsByTagName("td");
+            if (cells.length > 0) {
+                if (cells[2].innerText === array[index].email) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
--- assets/js/poo/main.js
+++ assets/js/poo/main.js
@@ -1,76 +1,71 @@
 //cobtrolador
-
-
-
-var persona = new Persona();
+var persona;
+var personas = [];
+var contador = 0;
+var table = document.getElementById("tableD");
+var rows = table.getElementsByTagName("tr");
 
 function enviarDatos() {
+    var idH = document.getElementById('cc').value;
+    var nombreH = document.getElementById('nombre').value;
+    var emailH = document.getElementById('email').value;
+    var claveH = document.getElementById('pwd').value;
+    var rolH = document.getElementById('rol').value;
 
-    let nombreH = document.getElementById("nombre").value;
-    let idH = document.getElementById("cc").value;
-    let emailH = document.getElementById("email").value;
-    let claveH = document.getElementById("pwd").value;
-    let rolH = document.getElementById("rol").value;
-    
-    if (nombreH!="" && idH!="" && emailH!="" && claveH!="" && rolH!="") {        
-        persona.id=idH;
-        persona.nombre=nombreH;
-        persona.email=emailH;
-        persona.clave=claveH;
-        persona.rol=rolH;
-        console.log(persona);
-        persona.guardar(persona);
-        console.log(persona.personas);
-        
-        persona.llenarObjeto();
-    }else{
-        alert("por favor revise los campos , no pueden estar vacios");
-    }    
-}
+    persona = new Persona(idH, nombreH, emailH, claveH, rolH);
+    personas.push(persona);
 
-function Editar() {
-// persona1.Editar(2,"andres","andres@jj.com",null,false);
-    var nombreH = document.getElementById("nombre").value;
-    var idH = document.getElementById("cc").value;
-    var emailH = document.getElementById("email").value;
-    var claveH = document.getElementById("pwd").value;
-    var rolH = document.getElementById("rol").value;
-    if (nombreH!="" && idH!="" && emailH!="" && claveH!="" && rolH!="") {
-        
-        persona.editar(idH,nombreH,emailH,claveH,rolH);        
-        console.log(persona);
-        console.log(persona.actualizarDatos(persona.personas));
-        
-    }else{
-        alert("por favor revise los campos , no pueden estar vacios");
+    if (nombreH !== "" && idH !== "" && emailH !== "" && claveH !== "" && rolH !== "") {
+        if (persona.buscar(personas, contador, rows)) {
+            contador += 1;
+            persona.llenarTabla(personas, table, (contador - 1));
+
+        } else {
+            alert("El Email ya ha sido registrado");
+            personas.splice(contador, 1);
+            return;
+        }
+    } else {
+        alert("Por favor, revise los campos. No pueden estar vacíos.");
     }
-    
-    // persona.ActualizarDatos(Persona.conteo)
-    
-    
 }
 
+function editar() {
+    var nuevoid = document.getElementById("cc").value;
+    var nuevoNombre = document.getElementById("nombre").value;
+    var nuevoEmail = document.getElementById("email").value;
+    var nuevaClave = document.getElementById("pwd").value;
+    var nuevoRol = document.getElementById("rol").value;
 
-function Eliminar() {
-    
-        
-    delete persona.nombre;
-    delete persona.id;
-    delete persona.email;
-    delete persona.clave;
-    delete persona.rol;
-    console.log(persona);
-    
-    
-
-
-
+    var personad = new Persona(nuevoid, nuevoNombre, nuevoEmail, nuevaClave, nuevoRol);
+    if (!persona.buscar(personas, (contador - 1), rows)) {
+        var p=persona.editarPersona(personad, personas, table);
+        if (p){
+            alert("los Emails no coinciden")
+        }
+    } else {
+        alert("No se puede editar un elemento inexistente. Se creará el elemento");
+        enviarDatos();
+    }
 }
 
-document.getElementById("btn").addEventListener('click',function() {
-    enviarDatos();
-})
+function eliminar(){
+    var nuevoid = document.getElementById("cc").value;
+    var nuevoNombre = document.getElementById("nombre").value;
+    var nuevoEmail = document.getElementById("email").value;
+    var nuevaClave = document.getElementById("pwd").value;
+    var nuevoRol = document.getElementById("rol").value;
 
-// btn.addEventListener('click',function() {
-//     alert("click");
-// })
+    var personad = new Persona(nuevoid, nuevoNombre, nuevoEmail, nuevaClave, nuevoRol);
+    if (!persona.buscar(personas, (contador - 1), rows)) {
+        var p=persona.eliminarPersona(personad, personas, table);
+        if (p){
+            alert("los Emails no coinciden")
+        }
+    } else {
+        alert("No se puede editar un elemento inexistente. Se creará el elemento");
+        enviarDatos();
+    }
+}
+
+document.getElementById("btn").addEventListener('click', enviarDatos);
